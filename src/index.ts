277b00be1393887a2c6146d688e115c04a9a8197
Mark Irvine <mark@wbb.ai>
2021-04-08 11:35:39 +0100

    index.ts: added simple express server to start the loop

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+import express from "express"
+import {startLoop} from "./arbitrage/arbitrage";
+
+const app = express()
+const port = 8080;
+
+// define a route handler for the default home page
+app.get( "/", ( _req, res ) => {
+  res.send( "Hello world!" );
+} );
+
+// start the Express server
+app.listen( port, async () => {
+  console.log( `server started at http://localhost:${ port }` );
+  await startLoop();
+} );
